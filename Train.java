1d22b5ed1fe2770ea6c9d341204d2f344c8b1b17
git-riyazuddin <113254834+git-riyazuddin@users.noreply.github.com>
2022-10-10 09:48:13 +0530

    Add files via upload

--- Train.java
+++ Train.java
@@ -1,31 +1,50 @@
-package zoho;
-import java.util.*;
-public class Train {
-public static void main(String[] args) {
-	Scanner s=new Scanner(System.in);
-	boolean loop=true;
-	while(loop)
-	{
-		System.out.println("1.Book \n2.Cancel \n3.Available Tickets \n4.Booked Tickets \5.Exit");
-		int choice=s.nextInt();
-		switch(choice) {
-		case 1:
+package riyaz;
+
+import java.util.Scanner;
+
+public class TrainRes {
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		boolean loop=true;
+		while(loop)
 		{
-			Book();
-			break;
-		}
-		
-		case 5:
-		{
-			loop=false;
-			break;
+			System.out.println("1.Book \n2.Cancel \n3.Available Tickets \n4.Booked Tickets \5.Exit");
+			int choice=s.nextInt();
+			switch(choice) {
+			case 1:
+			{
+				Book bo=new Book();
+				bo.Book();
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			case 3:
+			{
+				break;
+				
+			}
+			case 4:
+			{
+				break;
+			}
 			
-		}
+			case 5:
+			{
+				loop=false;
+				break;
+				
+			}
+			}
 		}
 	}
-}
+	
 }
 class Book{
+	
+	public void Book() {
 	Scanner s=new Scanner(System.in);
 	System.out.println("Enter the passenger Name:");
 	String name =s.next();
@@ -36,6 +55,58 @@ class Book{
 	System.out.println("Enter the passenger Bertn preference:(L,M,U)");
 	String bp=s.next();
 	Passenger p=new Passenger(name,age,gender,bp);
+	
+}
+public void Ticket() {
+	Scanner s=new Scanner(System.in);
+		String[][]str=new String[11][6];
+		int j=0;
+		int i=0;
+		str[i][j++]="PNR";
+		str[i][j++]="NAME";
+		str[i][j++]="AGE";
+		str[i][j++]="GENDER";
+		str[i][j++]="BERTH";
+		str[i][j++]="WHERE TO WHERE ";
+
+	
+		for(i=1;i<=10;i++)
+		{
+			for(j=1;j<6;j++) {
+				if(j==1)
+				{
+					System.out.println("Enter the passenger Name:");
+					str[i][j]=s.next();
+				}
+				if(j==2)
+				{
+					int n=s.nextInt();
+					str[i][j]=s.next();
+					
+				}
+				if(j==3)
+				{
+					str[i][j]=s.next();
+					
+				}
+				if(j==4)
+				{
+					str[i][j]=s.next();
+				}
+				if(j==5)
+				{
+				str[i][j]="EGMORE TO THIRUCHI";	
+				}
+
+			}
+			
+		}
+	}
+}
+class Cancel{
+	public Cancel() {
+		
+	}
 }
 class Passenger{
 	static int id=1;
